6a08eabe93bbd5574a316dd9eccd3b291fc41db4
Matías Stieben <matusanst@hotmail.com>
2025-05-21 18:46:48 -0300

    primer commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import HomeScreen from './src/screens/HomeScreen';
+import MarketScreen from './src/screens/MarketScreen';
+import AccountScreen from './src/screens/AccountScreen';
+import CartScreen from './src/screens/CartScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Inicio"
+        screenOptions={{ headerShown: false }}
+      >
+        <Drawer.Screen name="Inicio" component={HomeScreen} />
+        <Drawer.Screen name="Market" component={MarketScreen} />
+        <Drawer.Screen name="Mi Cuenta" component={AccountScreen} />
+        <Drawer.Screen name="Carrito" component={CartScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
